984cca5972fbfc4366e1a5d25217bca0f1be26e7
Przemysław Juśkiewicz <nyweron@gmail.com>
2021-01-16 14:42:29 +0100

    Added Settings for CatalogDatabaseSettings and add options to appsettings

--- src/Catalog/Catalog.API/Catalog.API/Startup.cs
+++ src/Catalog/Catalog.API/Catalog.API/Startup.cs
@@ -1,14 +1,10 @@
+using Catalog.API.Settings;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.Extensions.Options;
 
 namespace Catalog.API
 {
@@ -24,6 +20,13 @@ namespace Catalog.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // requires using Microsoft.Extensions.Options
+            services.Configure<CatalogDatabaseSettings>(
+                Configuration.GetSection(nameof(CatalogDatabaseSettings)));
+
+            services.AddSingleton<ICatalogDatabaseSettings>(sp =>
+                sp.GetRequiredService<IOptions<CatalogDatabaseSettings>>().Value);
+
             services.AddControllers();
         }
 
