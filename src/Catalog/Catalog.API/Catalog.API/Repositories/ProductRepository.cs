d9c4e1c7565989f7a8f30989db953dd39494a059
Przemysław Juśkiewicz <nyweron@gmail.com>
2021-01-16 15:15:03 +0100

    Add ProductRepository

---
+++ src/Catalog/Catalog.API/Catalog.API/Repositories/ProductRepository.cs
@@ -0,0 +1,55 @@
+﻿using Catalog.API.Data.interfaces;
+using Catalog.API.Entities;
+using Catalog.API.Repositories.interfaces;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly ICatalogContext _context;
+
+        public ProductRepository(ICatalogContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Product>> GetProducts()
+        {
+            return await _context.Products.Find(product => true).ToListAsync();
+        }
+
+        public Task<Product> GetProduct(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Product> GetProductByCategory(string categoryName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Product> GetProductByName(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Create(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Delete(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Update(Product product)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
