cc01e26670924d944a83165a93cc32c8d39d3f14
Przemysław Juśkiewicz <nyweron@gmail.com>
2021-01-17 15:37:18 +0100

    Added CatalogController

---
+++ src/Catalog/Catalog.API/Catalog.API/Controllers/CatalogController.cs
@@ -0,0 +1,31 @@
+﻿using Catalog.API.Entities;
+using Catalog.API.Repositories.interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+
+namespace Catalog.API.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class CatalogController : ControllerBase
+    {
+        private readonly IProductRepository _productRepository;
+        private readonly ILogger _logger;
+
+        public CatalogController(IProductRepository productRepository, ILogger logger)
+        {
+            _productRepository = productRepository;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Product>> GetProducts()
+        {
+            return await _productRepository.GetProducts();
+        }
+
+    }
+}
