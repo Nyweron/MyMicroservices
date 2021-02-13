d2308e2663e668d987deef9cedd4677d916e840b
Przemysław Juśkiewicz <nyweron@gmail.com>
2021-02-13 18:37:41 +0100

    Clean file

--- src/Ordering/Ordering.Infrastructure/Data/OrderContextSeed.cs
+++ src/Ordering/Ordering.Infrastructure/Data/OrderContextSeed.cs
@@ -1,9 +1,6 @@
 ﻿using Ordering.Core.Entities;
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Ordering.Infrastructure.Data
 {
