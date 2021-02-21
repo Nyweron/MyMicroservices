1e7df5ceb4664187be4a114239feba02c6c03684
Przemysław Juśkiewicz <nyweron@gmail.com>
2021-02-21 13:24:47 +0100

    Added automapper

---
+++ src/Ordering/Ordering.Application/Mapper/OrderMapperProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Ordering.Application.Responses;
+using Ordering.Core.Entities;
+
+namespace Ordering.Application.Mapper
+{
+    public class OrderMapperProfile : Profile
+    {
+        public OrderMapperProfile()
+        {
+            CreateMap<Order, OrderResponse>().ReverseMap();
+        }
+    }
+}
